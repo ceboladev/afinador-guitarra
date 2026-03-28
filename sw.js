34a7c9bbde6e56e3249e2833bdaec1b0efd37f93
Your Name <you@example.com>
2026-03-28 15:09:55 -0300

    modd

--- sw.js
+++ sw.js
@@ -7,3 +7,27 @@ self.addEventListener('fetch', (e) => {
   // Necessário para o PWA ser detectado como instalável
   e.respondWith(fetch(e.request));
 });
+
+const CACHE_NAME = 'tuner-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './icon-192.png'
+];
+
+// Instala e guarda os arquivos no cache do celular
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Responde com o cache se estiver offline
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
